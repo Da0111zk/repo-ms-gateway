8ddde3249641797aeb3dd96cd946f96c4263c165
Da0111zk <da.azocard@duocuc.cl>
2026-06-28 21:59:51 -0400

    feat: ms-gateway independiente

---
+++ src/test/java/ms/gateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package ms.gateway.gateway; 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.bodega.gateway.MsGatewayApplication; 
+
+@SpringBootTest(classes = MsGatewayApplication.class)
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
